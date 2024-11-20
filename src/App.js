322005cd01439ca4cf6bd0913c6d2a1bbea69953
RichardWMuller <richardwmuller@gmail.com>
2024-11-20 17:49:52 -0300

    News

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-import { useState } from 'react'
 
 function App() {
   return (
     <>
       <div className="page">
         <Header />
+
         <Main />
+
         <Footer />
       </div>
       <template id="elements__list" />
