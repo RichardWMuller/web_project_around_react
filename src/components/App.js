e70806f7e0f2593930c7d99d061cc5ae17f6510f
RichardWMuller <richardwmuller@gmail.com>
2024-11-22 17:47:23 -0300

    changes: moving positions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 function App() {
   return (
