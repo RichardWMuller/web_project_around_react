131b962721596df56b087990efcc4e4cd5e97a01
RichardWMuller <richardwmuller@gmail.com>
2024-11-22 17:49:09 -0300

    imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 function App() {
   return (
